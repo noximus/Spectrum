a52b54270477b24e1b4e560eb6568b17b4d841aa
schao87 <schaosti@gmail.com>
2015-10-29 15:07:19 -0400

    lightning

--- dev/300x250/Animation.js
+++ dev/300x250/Animation.js
@@ -62,7 +62,6 @@ var BanTimeline = {
       TweenLite.to(rain12, 1.3, {display:"block", top:300,left:-260, delay:2.2});
       TweenLite.to(rain13, 1.3, {display:"block", top:300,left:-260, delay:2.4});
       TweenLite.to(rain14, 1.3, {display:"block", top:300,left:-260, delay:2.6});
-      
     }
     TweenLite.delayedCall(2.3, BanTimeline.rain);
   },
@@ -71,12 +70,41 @@ var BanTimeline = {
     BanTimeline.glowBtn = 1;
     BanTimeline.rainOn = 1; 
     TweenLite.delayedCall(0, BanTimeline.rain);
-      
-      
-    // TweenLite.delayedCall(3, BanTimeline.frame2);
+    TweenLite.delayedCall(1, BanTimeline.frame2);
   },
   frame2: function () {
-    // TweenLite.to(f2_img1, 2, {opacity:1, display: 'block', delay: 0, ease: Quad.easeOut});
+    TweenLite.to(flick2, .1, {display:"block", delay:.3});
+    TweenLite.to(flick2, .1, {display:"none", delay:.45});
+    TweenLite.to(flick2, .1, {display:"block", delay:.6});
+    //clouds flickering
+    TweenLite.to(left_cloud, .1, {display:"block", opacity:.9, delay:.3});
+    TweenLite.to(left_cloud, .1, {display:"block", opacity:.6, delay:.4});
+    TweenLite.to(left_cloud, 1, {display:"block", opacity:0, delay:1});
+    //left lightning
+    TweenLite.to(left_light1, .1, {display:"block", delay:.3});
+    TweenLite.to(left_light2, .1, {display:"block", delay:.35});
+    TweenLite.to(left_light3, .1, {display:"block", delay:.4});
+    TweenLite.to(left_light1, .3, {display:"none",opacity:0, delay:.4});
+    TweenLite.to(left_light2, .3, {display:"none",opacity:0, delay:.4});
+    TweenLite.to(left_light3, .3, {display:"none",opacity:0, delay:.4});
+
+
+
+
+
+    TweenLite.to(right_cloud, .1, {display:"block", opacity:.9, delay:.6});
+    TweenLite.to(right_cloud, .1, {display:"block", opacity:.6, delay:.7});
+    TweenLite.to(right_cloud, 1, {display:"block", opacity:0, delay:1.3});
+
+
+
+
+    TweenLite.to(flick2, .1, {display:"none", delay:.7});
+    TweenLite.to(flick2, .05, {display:"block", delay:.75});
+    TweenLite.to(flick2, .03, {display:"none", delay:.8});
+    TweenLite.to(flick2, .05, {display:"block", delay:.9});
+
+
 
     // TweenLite.delayedCall(3, BanTimeline.frame3);
   },
