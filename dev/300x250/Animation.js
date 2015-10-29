f440f33ef1e572151e486d25ac9554b145eecf1b
schao87 <schaosti@gmail.com>
2015-10-29 11:21:54 -0400

    turned btn on and off

--- dev/300x250/Animation.js
+++ dev/300x250/Animation.js
@@ -1,28 +1,41 @@
 var BanTimeline = {
-  // f1_img1: document.getElementById("f1_img1"),
-  // f1_img2: document.getElementById("f1_img2"),
-  // f2_img1: document.getElementById("f2_img1"),
-  // f3_img1: document.getElementById("f3_img1"),
-  // f4_img1: document.getElementById("f4_img1"),
-  // f5_img1: document.getElementById("f5_img1"),
+  glowBtn: 0,
+  container: document.getElementById("container"),
   init: function () {
     // initial settings for banner frame 1.  In case banner needs to restart.  add as many as you can to CSS first so Banner doesn't jump
     document.getElementById("container").style.display = "block";
     // run animation after init is done. 
     BanTimeline.frame1();
   },
-
   reset: function () {
     
   },
-  btnGlow: function () {
-    
+
+  glow: function() {
+    if(BanTimeline.glowBtn == 1){
+      TweenLite.to(switch_now, 0, {display: 'block',opacity:1});
+      container.onmouseover = function(){
+        TweenLite.to(switch_glow1, 0.2, {display: 'block',opacity:.5});
+        TweenLite.to(switch_glow2, 0.2, {display: 'block',opacity:.5, delay:.1});
+        TweenLite.to(switch_glow3, 0.2, {display: 'block',opacity:.5, delay:.15});
+        TweenLite.to(switch_glow4, 0.2, {display: 'block',opacity:.5, delay:.2});
+        TweenLite.to(switch_glow5, 0.2, {display: 'block',opacity:.5, delay:.25});
+        TweenLite.to(switch_glow1, 0.2, {opacity:0, delay:.25});
+        TweenLite.to(switch_glow2, 0.2, {opacity:0, delay:.2});
+        TweenLite.to(switch_glow3, 0.2, {opacity:0, delay:.3});
+        TweenLite.to(switch_glow4, 0.2, {opacity:0, delay:.4});
+        TweenLite.to(switch_glow5, 0.2, {opacity:0, delay:.5});     
+      }
+    }
   },
+
   frame1: function () {
-    glow=1;
+    BanTimeline.glowBtn=0;
+    //BanTimeline.glow();
+    //console.log('frame1')
     // TweenLite.to(f1_img1, 2, {opacity:1, display: 'block', delay: 0, ease: Quad.easeOut, yoyo: true});
     // TweenLite.to(f1_img2, 2, {opacity:1, display: 'block', delay: 1, ease: Quad.easeOut, yoyo: true});
-  
+    // TweenLite.delayedCall(1, BanTimeline.glow);
     // TweenLite.delayedCall(3, BanTimeline.frame2);
   },
   frame2: function () {
@@ -44,21 +57,8 @@ var BanTimeline = {
     // TweenLite.to(f5_img1, 2, {opacity:1, display: 'block', delay: 0, ease: Quad.easeOut});
   }
 };
-container.onmouseover = function() {
-  if(glow = 1){
-      TweenLite.to(switch_glow1, 0.2, {opacity:.5});
-      TweenLite.to(switch_glow2, 0.2, {opacity:.5, delay:.1});
-      TweenLite.to(switch_glow3, 0.2, {opacity:.5, delay:.15});
-      TweenLite.to(switch_glow4, 0.2, {opacity:.5, delay:.2});
-      TweenLite.to(switch_glow5, 0.2, {opacity:.5, delay:.25});
 
-      TweenLite.to(switch_glow1, 0.2, {opacity:0, delay:.25});
-      TweenLite.to(switch_glow2, 0.2, {opacity:0, delay:.2});
-      TweenLite.to(switch_glow3, 0.2, {opacity:0, delay:.3});
-      TweenLite.to(switch_glow4, 0.2, {opacity:0, delay:.4});
-      TweenLite.to(switch_glow5, 0.2, {opacity:0, delay:.5}); 
-  }
-}; 
 BanTimeline.init();
+BanTimeline.glow();
 
 
